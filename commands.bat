2262f28013d66145f0b494c93026386184f17873
eli_shteinman <a0533057932@gmail.com>
2025-08-14 16:46:36 +0300

    Add batch script for project management and MySQL deployment

---
+++ commands.bat
@@ -0,0 +1,7 @@
+// מחיקת פרוייקט ישן
+oc delete project <project_name>
+// יצירת פרוייקט חדש
+oc new-project <project_name> --description="<description>" --display-name="<display_name>"
+// יצירת MYSQL
+oc new-app mysql-persistent --name mysql
+//
